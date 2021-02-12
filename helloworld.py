afe92ec39b4f61594db771493a85d8e5283fb406
Yash <furiousbluffer@gmail.com>
2021-02-12 22:06:04 +0530

    Clean up ignored files

--- helloworld.py
+++ helloworld.py
@@ -10,3 +10,12 @@ print(f"Value of c is {c}")
 # List comprehensions
 sq = [x*x for x in list(range(1, 11))]
 print(sq)
+
+# References
+L1 = ["a"]
+L2 = ["b"]
+L3 = L1
+L1.append("c")
+L3.extend(L2)
+# L3 isn't a separate list, just a reference to L1; any changes to L1 shall be reflected in L3 as well.
+print(L3)
