43b6026dd9a673624500e35d948ad4d929a14a08
Yash <furiousbluffer@gmail.com>
2021-02-12 21:46:02 +0530

    add gitignore; used list comprehensions

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,12 @@
+# Hello World!
 print("Hello World!")
-
 a = 3 + 4
 b = 3 % 4
+c = 4 + 6
 
-c = 4+6
-
+# f-strings
 print(f"Value of c is {c}")
+
+# List comprehensions
+sq = [x*x for x in list(range(1, 11))]
+print(sq)
